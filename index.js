590d3963829ef02f0f29d25d6d6e522b9d9af188
Aleksandr Malakhanov <aleksandr.malakhanov@customertimes.com>
2014-12-30 11:51:06 +0200

    resources for model references

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var underscore = require("underscore");
 var common = require("./common.js");
-var mongoose = require('mongoose');
 
 var defaultOptions = {};
 
@@ -16,6 +15,9 @@ function NotFoundError() {
   return this;
 }
 
+var backRefs = {};
+var initCache = {};
+
 /**
  * Get new resource controller
  * @param model Mongoose model for resource to work with
@@ -25,6 +27,14 @@ function NotFoundError() {
 module.exports = function (app, model, resourceOptions) {
 
   var options = underscore.extend(underscore.clone(defaultOptions), resourceOptions);
+
+  var modelCache = {};
+
+  initCache[model.modelName] = {
+    app: app,
+    model: model,
+    options: options
+  };
 
   var resources = findSubDocuments(model.schema, [options.name]);
   resources.unshift({
@@ -53,8 +63,23 @@ module.exports = function (app, model, resourceOptions) {
       if (value instanceof Array && value.length === 1 && value[0].constructor.name === "Schema") {
         var elementPath = underscore.clone(path);
         elementPath.push(i);
-        children.push({path: elementPath, schema: value[0]});
+        children.push({path: elementPath, schema: value[0], type: "sub"});
         Array.prototype.push.apply(children, findSubDocuments(value[0], elementPath));
+      } else if (value.type && value.type.name === "ObjectId" && value.ref) {
+        if (!options.modelResolver) {
+          throw new Error("No model resolver in options");
+        }
+        modelCache[value.ref] = options.modelResolver(value.ref);
+        var elementPath = underscore.clone(path);
+        elementPath.push(i.replace(/id$/i, ""));
+        children.push({path: elementPath, field: i, schema: modelCache[value.ref].schema, type: "ref"});
+        //if(!backRefs[value.ref]) {
+        //  backRefs[value.ref] = [];
+        //}
+        //backRefs[value.ref].push({
+        //  model: model,
+        //  field: i
+        //});
       }
     }
     return children;
@@ -232,198 +257,222 @@ module.exports = function (app, model, resourceOptions) {
 
     var controller = {};
 
-    if (resource.ids.length > 1) {
-      controller.index = function (req, res) {
-        checkParams(req, res, resource, resource.path.length - 1, function (params) {
-          model.findById(params[0], function (err, doc) {
-            handleErrors(err, resource.ids[0], doc, res, function () {
-              var sub;
-              try {
-                sub = getSubs(params, resource, doc);
-              } catch (ex) {
-                if (!(ex instanceof NotFoundError)) {
-                  throw ex;
+    function index(req, res, doc) {
+
+    }
+
+    switch (resource.type) {
+      case "sub":
+      {
+
+        controller.index = function (req, res) {
+          checkParams(req, res, resource, resource.path.length - 1, function (params) {
+            model.findById(params[0], function (err, doc) {
+              handleErrors(err, resource.ids[0], doc, res, function () {
+                var sub;
+                try {
+                  sub = getSubs(params, resource, doc);
+                } catch (ex) {
+                  if (!(ex instanceof NotFoundError)) {
+                    throw ex;
+                  }
+                  return common.handleError(res, ex.message + " not found", 404);
                 }
-                return common.handleError(res, ex.message + " not found", 404);
-              }
-              var start = req.query.start,
-                length = req.query.length;
-              if (start || length) {
-                sub = sub.slice(start ? start : 0, length ? (start + length) : sub.length);
-              }
-              return common.handleSuccess(res, format(sub, fieldLimitOptions(req, resource)), {
-                recordsFiltered: sub.length,
-                recordsTotal: sub.length
+                var start = req.query.start,
+                  length = req.query.length;
+                if (start || length) {
+                  sub = sub.slice(start ? start : 0, length ? (start + length) : sub.length);
+                }
+                return common.handleSuccess(res, format(sub, fieldLimitOptions(req, resource)), {
+                  recordsFiltered: sub.length,
+                  recordsTotal: sub.length
+                });
               });
             });
           });
-        });
-      };
-    } else {
-      controller.index = function (req, res) {
-        var queryOptions = { //TODO: make same DT logic for subs
-          skip: req.query.start,
-          limit: req.query.length
         };
-        if (req.query.order) {
-          queryOptions.sort = {};
-          req.query.order.forEach(function (order) {
-            queryOptions.sort[req.query.columns[order.column].data] = order.dir === "desc" ? -1 : 1;
+
+        controller.one = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findById(params[0], function (err, doc) {
+              handleErrors(err, resource.ids[0], doc, res, function () {
+                var sub;
+                try {
+                  sub = getSub(params, resource, doc);
+                } catch (ex) {
+                  if (!(ex instanceof NotFoundError)) {
+                    throw ex;
+                  }
+                  return common.handleError(res, ex.message + " not found", 404);
+                }
+                return common.handleSuccess(res, formatOne(sub, fieldLimitOptions(req, resource)));
+              });
+            });
           });
-        }
-        var query = {};
-        if (req.query.search && req.query.search.value && req.query.columns) {
-          var search = query.$or = [];
-            req.query.columns.forEach(function(field) {
+        };
+
+        controller.create = function (req, res) {
+          checkParams(req, res, resource, resource.path.length - 1, function (params) {
+            model.findById(params[0], function (err, doc) {
+              handleErrors(err, model.modelName, doc, res, function () {
+                var sub;
+                try {
+                  sub = getSubs(params, resource, doc);
+                } catch (ex) {
+                  if (!(ex instanceof NotFoundError)) {
+                    throw ex;
+                  }
+                  return common.handleError(res, ex.message + " not found", 404);
+                }
+                sub.push(validate(res, req.body));
+                return saveDoc(res, doc);
+              });
+            });
+          });
+        };
+
+        controller.update = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findById(params[0], function (err, doc) {
+              handleErrors(err, resource.ids[0], doc, res, function () {
+                var sub;
+                try {
+                  sub = getSub(params, resource, doc);
+                } catch (ex) {
+                  if (!(ex instanceof NotFoundError)) {
+                    throw ex;
+                  }
+                  return common.handleError(res, ex.message + " not found", 404);
+                }
+                underscore.extend(sub, validate(res, req.body));
+                return saveDoc(res, doc);
+              });
+            });
+          });
+        };
+
+        controller.remove = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findById(params[0], function (err, doc) {
+              handleErrors(err, resource.ids[0], doc, res, function () {
+                var sub;
+                try {
+                  sub = getSub(params, resource, doc);
+                } catch (ex) {
+                  if (!(ex instanceof NotFoundError)) {
+                    throw ex;
+                  }
+                  return common.handleError(res, ex.message + " not found", 404);
+                }
+                sub.remove();
+                return saveDoc(res, doc);
+              });
+            });
+          });
+        };
+
+        break;
+      }
+      case "ref":
+      {
+        
+        controller.index = function (req, res) {
+          checkParams(req, res, resource, resource.path.length - 1, function (params) {
+            var limit = {"_id": 1};
+            limit[resource.field] = 1;
+            model.findById(params[0], limit).populate(resource.field).lean().exec(function (err, doc) {
+              doc = doc[resource.field];
+              handleErrors(err, model.modelName, doc, res, function () {
+                var formatted = formatOne(doc, fieldLimitOptions(req, resource));
+                if (!formatted) {
+                  return common.handleError(res, "You can't get this " + resource.path[resource.path.length - 1], 400);
+                }
+                return common.handleSuccess(res, formatted);
+              });
+            });
+          });
+        };
+
+        break;
+      }
+      default:
+      {
+
+        controller.index = function (req, res) {
+          var queryOptions = { //TODO: make same DT logic for subs
+            skip: req.query.start,
+            limit: req.query.length
+          };
+          if (req.query.order) {
+            queryOptions.sort = {};
+            req.query.order.forEach(function (order) {
+              queryOptions.sort[req.query.columns[order.column].data] = order.dir === "desc" ? -1 : 1;
+            });
+          }
+          var query = {};
+          if (req.query.search && req.query.search.value && req.query.columns) {
+            var search = query.$or = [];
+            req.query.columns.forEach(function (field) {
               var name = field.data;
-              debugger;
               if (model.schema.tree[name] && model.schema.tree[name].type && model.schema.tree[name].type.name === "String") {
                 var fieldSearch = {};
                 fieldSearch[name] = new RegExp(req.query.search.value, "i");
                 search.push(fieldSearch);
               }
             });
-        }
-        model.find(query, fieldLimitOptions(req, resource), queryOptions).lean().exec(function (err, result) {
-          if (err) {
-            return common.handleError(res, err, 400);
           }
-          if (req.query.draw) {
-            result.draw = req.query.draw;
-          }
-          model.count({}, function (err, count) {
-            return common.handleSuccess(res, format(result, null), {
-              recordsFiltered: result.length,
-              recordsTotal: count
+          model.find(query, fieldLimitOptions(req, resource), queryOptions).lean().exec(function (err, result) {
+            if (err) {
+              return common.handleError(res, err, 400);
+            }
+            if (req.query.draw) {
+              result.draw = req.query.draw;
+            }
+            model.count(query, function (err, count) {
+              return common.handleSuccess(res, format(result, null), {
+                recordsFiltered: result.length,
+                recordsTotal: count
+              });
             });
           });
-        });
-      };
-    }
+        };
 
-
-    if (resource.ids.length > 1) {
-      controller.one = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findById(params[0], function (err, doc) {
-            handleErrors(err, resource.ids[0], doc, res, function () {
-              var sub;
-              try {
-                sub = getSub(params, resource, doc);
-              } catch (ex) {
-                if (!(ex instanceof NotFoundError)) {
-                  throw ex;
+        controller.one = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findById(params[0], fieldLimitOptions(req, resource)).lean().exec(function (err, doc) {
+              handleErrors(err, model.modelName, doc, res, function () {
+                var formatted = formatOne(doc);
+                if (!formatted) {
+                  return common.handleError(res, "You can't get this " + model.modelName, 400);
                 }
-                return common.handleError(res, ex.message + " not found", 404);
-              }
-              return common.handleSuccess(res, formatOne(sub, fieldLimitOptions(req, resource)));
+                return common.handleSuccess(res, formatted);
+              });
             });
           });
-        });
-      };
-    } else {
-      controller.one = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findById(params[0], fieldLimitOptions(req, resource)).lean().exec(function (err, doc) {
-            handleErrors(err, model.modelName, doc, res, function () {
-              var formatted = formatOne(doc);
-              if (!formatted) {
-                return common.handleError(res, "You can't get this " + Model.modelName, 400);
-              }
-              return common.handleSuccess(res, formatted);
+        };
+
+        controller.create = function (req, res) {
+          return saveDoc(res, new model(validate(res, req.body)));
+        };
+
+        controller.update = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findById(params[0], function (err, doc) {
+              return saveDoc(res, underscore.extend(doc, validate(res, req.body)));
             });
           });
-        });
-      };
-    }
+        };
 
-
-    if (resource.ids.length > 1) {
-      controller.create = function (req, res) {
-        checkParams(req, res, resource, resource.path.length - 1, function (params) {
-          model.findById(params[0], function (err, doc) {
-            handleErrors(err, model.modelName, doc, res, function () {
-              var sub;
-              try {
-                sub = getSubs(params, resource, doc);
-              } catch (ex) {
-                if (!(ex instanceof NotFoundError)) {
-                  throw ex;
-                }
-                return common.handleError(res, ex.message + " not found", 404);
-              }
-              sub.push(validate(res, req.body));
-              return saveDoc(res, doc);
+        controller.remove = function (req, res) {
+          checkParams(req, res, resource, resource.path.length, function (params) {
+            model.findOneAndRemove(params[0], function (err, doc) {
+              return common.handleSuccess(res);
             });
           });
-        });
-      };
-    } else {
-      controller.create = function (req, res) {
-        return saveDoc(res, new model(validate(res, req.body)));
-      };
-    }
+        };
 
-
-    if (resource.ids.length > 1) {
-      controller.update = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findById(params[0], function (err, doc) {
-            handleErrors(err, resource.ids[0], doc, res, function () {
-              var sub;
-              try {
-                sub = getSub(params, resource, doc);
-              } catch (ex) {
-                if (!(ex instanceof NotFoundError)) {
-                  throw ex;
-                }
-                return common.handleError(res, ex.message + " not found", 404);
-              }
-              underscore.extend(sub, validate(res, req.body));
-              return saveDoc(res, doc);
-            });
-          });
-        });
-      };
-    } else {
-      controller.update = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findById(params[0], function (err, doc) {
-            return saveDoc(res, underscore.extend(doc, validate(res, req.body)));
-          });
-        });
-      };
-    }
-
-
-    if (resource.ids.length > 1) {
-      controller.remove = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findById(params[0], function (err, doc) {
-            handleErrors(err, resource.ids[0], doc, res, function () {
-              var sub;
-              try {
-                sub = getSub(params, resource, doc);
-              } catch (ex) {
-                if (!(ex instanceof NotFoundError)) {
-                  throw ex;
-                }
-                return common.handleError(res, ex.message + " not found", 404);
-              }
-              sub.remove();
-              return saveDoc(res, doc);
-            });
-          });
-        });
-      };
-    } else {
-      controller.remove = function (req, res) {
-        checkParams(req, res, resource, resource.path.length, function (params) {
-          model.findOneAndRemove(params[0], function (err, doc) {
-            return common.handleSuccess(res);
-          });
-        });
-      };
+        break;
+      }
     }
 
     return controller;
@@ -440,11 +489,21 @@ module.exports = function (app, model, resourceOptions) {
       prefix += resource.path[i] + '/:' + resource.ids[i] + '/';
     }
 
-    app.route(prefix + lastPathName).get(resource.controller.index);
-    app.route(prefix + lastPathName).post(resource.controller.create);
-    app.route(prefix + lastPathName + '/:' + lastId).get(resource.controller.one);
-    app.route(prefix + lastPathName + '/:' + lastId).put(resource.controller.update);
-    app.route(prefix + lastPathName + '/:' + lastId).delete(resource.controller.remove);
+    if (resource.controller.index) {
+      app.route(prefix + lastPathName).get(resource.controller.index);
+    }
+    if (resource.controller.create) {
+      app.route(prefix + lastPathName).post(resource.controller.create);
+    }
+    if (resource.controller.one) {
+      app.route(prefix + lastPathName + '/:' + lastId).get(resource.controller.one);
+    }
+    if (resource.controller.update) {
+      app.route(prefix + lastPathName + '/:' + lastId).put(resource.controller.update);
+    }
+    if (resource.controller.remove) {
+      app.route(prefix + lastPathName + '/:' + lastId).delete(resource.controller.remove);
+    }
 
     return resource;
   }
